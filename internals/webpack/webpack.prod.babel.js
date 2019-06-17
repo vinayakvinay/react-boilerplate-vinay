43a2eeb8ce76c71651bb5715f96fece40ced6b1e
Vinay <Vinay>
2019-06-17 21:03:35 +0530

    misc

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -116,9 +116,9 @@ module.exports = require('./webpack.base.babel')({
     }),
 
     new WebpackPwaManifest({
-      name: 'React Boilerplate',
-      short_name: 'React BP',
-      description: 'My React Boilerplate-based project!',
+      name: 'VZ React Boilerplate',
+      short_name: 'VZ React App',
+      description: 'VZ React Boilerplate-based project!',
       background_color: '#fafafa',
       theme_color: '#b1624d',
       inject: true,
